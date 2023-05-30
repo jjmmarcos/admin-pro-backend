e5c93dc5fe2fb445bb5f2537b7770ae7b2fcab4f
jjmmarcos <javierbulevar8@gmail.com>
2023-05-30 19:42:54 +0200

    node backend for advanced angular course

---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+/*
+    Path: '/api/login'
+*/
+const { Router } = require('express');
+const { login, googleSignIn } = require('../controllers/auth');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+
+const router = Router();
+
+
+router.post( '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    login
+);
+
+router.post( '/google',
+    [
+        check('token', 'El token de google es obligatorio').not().isEmpty(),
+        validarCampos
+    ],
+    googleSignIn
+);
+
+
+
+
+
+
+module.exports = router;
